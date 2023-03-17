8c99aea76997c43665ab817fbb572b0442fbda79
kevin-magu <wanjaukevinmagu@gmail.com>
2023-03-17 17:00:05 +0300

    added some few files

---
+++ index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <style>
+    @import url('https://fonts.googleapis.com/css2?family=Kaushan+Script&display=swap');
+    </style>
+    <script src="https://kit.fontawesome.com/e4c074505f.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="index.css">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>FarmFresh Market</title>
+</head>
+<body>
+    <nav>
+        <b>FarmFresh Market</b>
+        <ul>
+           <a href=""><li>Shop</li></a> 
+           <a href=""><li>Orders</li></a> 
+           <a href=""><li>About</li></a> 
+           <a href=""><li>Contact</li></a> 
+        </ul>
+    </nav>
+
+    <div class="wrapper">
+    <div class="veggies">
+    <div id="veggie1" class="veggie-div">
+        <p>Fresh Kales</p>
+    </div>
+    <div id="veggie2" class="veggie-div"></div>    
+    </div>
+    <div class="advert">
+        <p>Sell your bountiful harvest or purchase high-quality farm products from our top rated website !</p>
+        <div class="stars">
+        <i class="fa-solid fa-star"></i>
+        <i class="fa-solid fa-star"></i>
+        <i class="fa-solid fa-star"></i>
+        <i class="fa-solid fa-star"></i>
+        <i class="fa-solid fa-star"></i>
+        </div>
+        <div class="buttons">
+        <button>Shop Now !</button>
+        <button>Sell Now !</button>
+        </div>
+    </div>
+    </div>
+</body>
+</html>
